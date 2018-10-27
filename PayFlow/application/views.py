8a84af8a925d497c62894f52aa912054b7acc012
Andreas Lordos <andreasglordos@gmail.com>
2018-10-27 18:57:20 +0300

    Added comments

--- PayFlow/application/views.py
+++ PayFlow/application/views.py
@@ -1,3 +1,8 @@
+'''
+authors:
+    Andreas Lordos (17, English School of Nicosia)
+    Christos Falas(16, English School of Nicosia)
+'''
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,16 +13,17 @@ from .registration import register
 import logging
 import os
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO) #sets up logging module
 logger = logging.getLogger(__name__)
 
-sessions=[]
+sessions=[] #initializes sessions
 webhook_secret='DPH49NT7DAR2D2FFZ99WGLHULWM3MZ7H'
-os.chdir("application")
+os.chdir("application") #to be in the same dir as users.json
 
 @csrf_exempt
 def index(request):
-    current_code=randint(300000,400000)
+    current_code=randint(300000,999999) #generate code for session that will be used to verify receiver and sender
+    #However, current_code would be implemented in a way that guarantees there will not be a collision if this was deployed in the real world
     logging.info(request.POST.get('event'))
 
     def generateCode(content,current_code,phone):
@@ -29,12 +35,12 @@ def index(request):
         Output:
             fullstr,String
         '''
-        money=str(content.split()[1])
+        money=str(content.split()[1]) #Get the amount that the receiver wants to receive from sender
         logging.info("Current code:")
         logging.info(current_code)
-        sessions.append(Session(current_code,int(money),phone))
-        fullstr="To get "+str(money)+" euros from your friend, they need to send the code "+str(current_code)+" to the phone number: 99795260"
-        return fullstr
+        sessions.append(Session(current_code,int(money),phone)) #Create a new Session instance, add it to the array of current sessions
+        fullstr="To get "+str(money)+" euros from your friend, they need to send the code "+str(current_code)+" to the phone number: 99795260" #This is what will be returned to the user
+        return sessions,fullstr
 
     def payment(receiver_phone,sender_phone,amount):
         '''
@@ -45,7 +51,7 @@ def index(request):
         Output:
             fullstr,String
         '''
-        pass
+        raise NotImplementedError
 
     def isCurrency(a_str):
         '''
@@ -55,11 +61,11 @@ def index(request):
             True/False,Bool
         '''
         try:
-            if float(a_str)>0:
+            if float(a_str)>0: #If it's a float or integer in the form of a string, this operation will work
                 return True
-        except:
+        except: #if the above operations raises an error, return False
             pass
-        return False
+        return False #edge case if a_str is a number less than or equal to 0
 
     def isRegistered(phone_number):
         '''
@@ -69,9 +75,9 @@ def index(request):
             True/False,Bool
         '''
         try:
-            with open('users.json') as json_file:
-                data = json.load(json_file)
-            if phone_number in data:
+            with open('users.json') as json_file: #open users.json file
+                data = json.load(json_file) #load data from file
+            if phone_number in data: #if phone number already in records
                 return True
         except:
             pass
@@ -95,25 +101,20 @@ def index(request):
             phone_id = request.POST.get('phone_id')
             logging.info("Content: "+content)
             if content.split()[0]=="gen" and isCurrency(content.split()[1]): #implement NLP later
-                if isRegistered(from_number):
-                    fullstr=generateCode(content,current_code,from_number)
-                else:
+                if not isRegistered(from_number):
                     logging.info(os.getcwd())
                     register(from_number)
-                    fullstr=generateCode(content,current_code,from_number)
+                sessions,fullstr=generateCode(content,current_code,from_number)
             elif content.split()[0].isdigit() and len(content.split())==0:
                 done=False
                 requested_id=int(content.split()[0])
                 for session in sessions:
                     if session.id==requested_id:
                         done=True
-                        if isRegistered(from_number):
-                            #payment()
-                            pass
-                        else:
+                        if not isRegistered(from_number):
                             register(from_number)
-                            #payment()
-                            pass
+                        payment(session.receiver,session.sender,session.amount)
+                        sessions.remove(session)
                 if done==False:
                     fullstr="Invalid ID sent. Please try again."
             else:
